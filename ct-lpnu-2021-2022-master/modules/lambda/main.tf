64f2b059f66cf202e0a33a041d26ed2bb7b5632e
Yura <yrameda0404@gmail.com>
2022-06-09 22:45:48 +0300

    rev

--- ct-lpnu-2021-2022-master/modules/lambda/main.tf
+++ ct-lpnu-2021-2022-master/modules/lambda/main.tf
@@ -72,6 +72,103 @@ module "lambda_function_get_all_courses" {
   tags = module.label.tags
 }
 
+module "lambda_function_get_course" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "3.2.0"
+
+  # function_name                     = "${module.label.id}-get-course"
+  function_name                     = module.label_get_course.id
+  description                       = "get  course"
+  handler                           = "index.handler"
+  runtime                           = "nodejs12.x"
+  create_role                       = false
+  lambda_role                       = var.get_course_role_arn
+  use_existing_cloudwatch_log_group = true
+  cloudwatch_logs_retention_in_days = 3
+
+
+  source_path = "${path.module}/lambda_source/get_course/get-course.js"
+
+  environment_variables = {
+    TABLE_NAME = var.dynamo_db_courses_name
+  }
+
+  tags = module.label.tags
+}
+
+
+module "lambda_function_save_course" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "3.2.0"
+
+  # function_name                     = "${module.label.id}-save-course"
+  function_name                     = module.label_save_course.id
+  description                       = "save course"
+  handler                           = "index.handler"
+  runtime                           = "nodejs12.x"
+  create_role                       = false
+  lambda_role                       = var.save_course_role_arn
+  use_existing_cloudwatch_log_group = true
+  cloudwatch_logs_retention_in_days = 3
+
+
+  source_path = "${path.module}/lambda_source/save_course/save-course.js"
+
+  environment_variables = {
+    TABLE_NAME = var.dynamo_db_courses_name
+  }
+
+  tags = module.label.tags
+}
+
+module "lambda_function_delete_course" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "3.2.0"
+
+  # function_name                     = "${module.label.id}-delete-course"
+  function_name                     = module.label_delete_course.id
+  description                       = "delete course"
+  handler                           = "index.handler"
+  runtime                           = "nodejs12.x"
+  create_role                       = false
+  lambda_role                       = var.delete_course_role_arn
+  use_existing_cloudwatch_log_group = true
+  cloudwatch_logs_retention_in_days = 3
+
+
+  source_path = "${path.module}/lambda_source/save_course/delete-course.js"
+
+  environment_variables = {
+    TABLE_NAME = var.dynamo_db_courses_name
+  }
+
+  tags = module.label.tags
+}
+
+module "lambda_function_update_course" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "3.2.0"
+
+  # function_name                     = "${module.label.id}-update-course"
+  function_name                     = module.label_update_course.id
+  description                       = "update course"
+  handler                           = "index.handler"
+  runtime                           = "nodejs12.x"
+  create_role                       = false
+  lambda_role                       = var.update_course_role_arn
+  use_existing_cloudwatch_log_group = true
+  cloudwatch_logs_retention_in_days = 3
+
+
+  source_path = "${path.module}/lambda_source/save_course/update-course.js"
+
+  environment_variables = {
+    TABLE_NAME = var.dynamo_db_courses_name
+  }
+
+  tags = module.label.tags
+}
+
 resource "aws_cloudwatch_log_group" "get_all_authors" {
   name              = format("/aws/lambda/%s", module.label_get_all_authors.id)
   retention_in_days = local.log_retention
--- ct-lpnu-2021-2022-master/modules/lambda/variables.tf
+++ ct-lpnu-2021-2022-master/modules/lambda/variables.tf
@@ -6,13 +6,27 @@ variable "dynamo_db_courses_name" {
   type = string
 }
 
+
+
 variable "get_all_authors_role_arn" {
   type = string
 }
-
 variable "get_all_courses_role_arn" {
   type = string
 }
+# variable "get_course_role_arn" {
+#   type = string
+# }
+# variable "save_course_role_arn" {
+#   type = string
+# }
+# variable "update_course_role_arn" {
+#   type = string
+# }
+# variable "delete_course_role_arn" {
+#   type = string
+# }
+
 
 variable "aws_lambda_permission_api_gateway_source_arn" {
   type = string
